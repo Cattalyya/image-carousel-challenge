819c43759c8c4f9e3d3e79fd140b4233ac5c4261
Cattalyya Nuengsigkapian <cattalyya@hotmail.com>
2017-02-24 18:48:29 -0500

    Implement Indicators to trace our position of current images, and styling them using css icon from a third party FontAwesome.

--- js/carouselController.js
+++ js/carouselController.js
@@ -1,17 +1,17 @@
 angular.module('mainCarousel',[]).controller('CarouselController', function ($scope) {
 
   var imageFolderPath = "img/";
-  var imageFilenames = ["1.jpg", "2.jpg", "3.jpg"];
+  var imageFilenames = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"];
 
   var DEFAULT_SELECTED_IMAGE_INDEX = 0;
   var selectedIndex;
-  var numOfImages = 0;
-
+  
+  $scope.totalImages = 0;
   $scope.currentImgSrc = null;
   $scope.title = "KitCat Photography Album"
 
   var initialize = function(){
-      numOfImages = imageFilenames.length;
+      $scope.totalImages = imageFilenames.length;
       selectedIndex = DEFAULT_SELECTED_IMAGE_INDEX;
       $scope.currentImgSrc = "img/1.jpg";
   };
@@ -23,17 +23,27 @@ angular.module('mainCarousel',[]).controller('CarouselController', function ($sc
   initialize();
 
   $scope.next = function() {
-      selectedIndex = (selectedIndex+1) % numOfImages;
+      selectedIndex = (selectedIndex+1) % $scope.totalImages;
       update();
   };  
 
   $scope.prev = function() {
       if (selectedIndex == 0) {
-          selectedIndex = numOfImages-1;
+          selectedIndex = $scope.totalImages-1;
       } else  {
           selectedIndex = selectedIndex-1;
       }
       update();
   }; 
+
+  $scope.getIndicatorClasses= function(){
+      var tmp= imageFilenames.map( function(element, index){
+          if (index == selectedIndex){
+              return "fa fa-circle"
+          } 
+          return "fa fa-circle-o";
+      });
+      return tmp;
+  };
 
 });
